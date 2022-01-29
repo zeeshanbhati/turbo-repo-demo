6a90a1bac18e04eed978dc8e4b67e3b64ff8f7a4
Zeeshan Bhati <zeeshan.bhati@useplaza.com>
2022-01-29 21:28:14 +0530

    Shared Tailwind Added

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button className="px-4 py-2 rounded-md bg-blue-500 text-black">Boop</button>;
 };
