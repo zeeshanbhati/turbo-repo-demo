0145c56656b783f81b881527655e271e23d88f15
Zeeshan Bhati <zeeshan.bhati@useplaza.com>
2022-01-29 21:36:37 +0530

    Added New next app

---
+++ apps/next-demo/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+
+const withTM = require("next-transpile-modules")(["ui"]);
+
+
+module.exports = withTM({
+  reactStrictMode: true,
+})
---
+++ apps/next-demo/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'ui'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="p-4 text-2xl">Turbo repo Demo App </h1>
+      <Button />
+    </div>
+  )
+}
