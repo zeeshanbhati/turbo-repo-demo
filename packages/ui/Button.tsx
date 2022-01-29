afd9b9b025c5a79c7d1fa2ad263da56fe719aace
Zeeshan Bhati <zeeshanbhati99@gmail.com>
2022-01-29 21:40:58 +0530

    Update Button.tsx

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,8 @@
 import * as React from "react";
 export const Button = () => {
-  return <button className="px-4 py-2 rounded-md bg-blue-500 text-black">Boop</button>;
+  return (
+    <button className="rounded-md bg-blue-500 px-4 py-2 text-black">
+      Boop
+    </button>
+  );
 };
