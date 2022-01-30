6825ef145afca45721a80c52b834c9160248c60a
Zeeshan Bhati <zeeshan.bhati@useplaza.com>
2022-01-30 19:54:24 +0530

    Vercel preview

--- apps/next-demo/pages/index.tsx
+++ apps/next-demo/pages/index.tsx
@@ -3,11 +3,13 @@ import { Button } from 'ui'
 export default function Home() {
   const getPikachu = () => {
     fetch(`${process.env.API_URL}`)
+    console.log('Testing for vercel preview')
   }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="p-4 text-2xl">Turbo repo Demo App </h1>
+
       <Button />
       <button
         onClick={getPikachu}
