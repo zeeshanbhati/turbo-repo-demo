a8baf6a9783c87a402a8bf035ecd90d181c233ae
Zeeshan Bhati <zeeshan.bhati@useplaza.com>
2022-01-30 19:41:13 +0530

    Pokemon API added for env testing

--- apps/next-demo/next.config.js
+++ apps/next-demo/next.config.js
@@ -2,7 +2,12 @@
 
 const withTM = require("next-transpile-modules")(["ui"]);
 
+const modulesExport = {
+  env: {
+    API_URL: process.env.API_URL
+  }
+}
 
-module.exports = withTM({
+module.exports = withTM(modulesExport, {
   reactStrictMode: true,
 })
--- apps/next-demo/pages/index.tsx
+++ apps/next-demo/pages/index.tsx
@@ -1,10 +1,20 @@
 import { Button } from 'ui'
 
 export default function Home() {
+  const getPikachu = () => {
+    fetch(`${process.env.API_URL}`)
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="p-4 text-2xl">Turbo repo Demo App </h1>
       <Button />
+      <button
+        onClick={getPikachu}
+        className="mt-4 rounded-md bg-yellow-500 px-4 py-2 text-black"
+      >
+        Use Pokemon API
+      </button>
     </div>
   )
 }
